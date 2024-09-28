31bd3f4b16425273ca72d065af352cd0e385d200
kushal-10 <kushalkoshti11@gmail.com>
2024-09-28 21:24:41 +0200

    rm flash attn req

--- create_boards.sh
+++ create_boards.sh
@@ -1,1 +1,1 @@
-python3 dataset/create_boards.py --board_size 15 --num_pieces 4 --num_boards 100 --level 'sample' --path 'data' --shapes "P T X Z U W" --colours "red blue green yellow magenta cyan"
+python3 dataset/create_boards.py --board_size 15 --num_pieces 4 --num_boards 10 --level 'sample' --path 'data' --shapes "P T X Z U W" --colours "red blue green yellow magenta cyan"
