25143b96e98760804464588c588314a4fa3d9a63
kushal-10 <kushalkoshti11@gmail.com>
2024-10-01 07:31:27 +0200

    add inference sript

--- dataset/generate_episodes.py
+++ dataset/generate_episodes.py
@@ -107,7 +107,7 @@ def episodes(level='sample', board_size=15):
     with open(training_metadata_path, 'w') as f:
         json.dump(metadata, f, indent=4)
 
-    print(f'Generated and saved instances for Level: {level}')
+    print(f"Generated and saved instances for Level: {level}")
 
 
 if __name__ == '__main__':
---
+++ inference_utils/infer.py
@@ -0,0 +1,45 @@
+from transformers import AutoProcessor, BitsAndBytesConfig, LlavaForConditionalGeneration
+import torch
+from datasets import load_from_disk
+import os
+
+LEVEL = "easy"
+
+MAX_LENGTH = 384
+REPO_ID = f"Koshti10/llava-1.5-7b-ft-{LEVEL}"
+MODEL_ID = "llava-hf/llava-1.5-7b-hf"
+hf_dataset = load_from_disk(os.path.join('training_data', f'hf_dataset_{LEVEL}'))
+test_dataset = hf_dataset['test']
+
+processor = AutoProcessor.from_pretrained(MODEL_ID)
+
+# Define quantization config
+quantization_config = BitsAndBytesConfig(
+    load_in_4bit=True, bnb_4bit_quant_type="nf4", bnb_4bit_compute_dtype=torch.float16
+)
+# Load the base model with adapters on top
+model = LlavaForConditionalGeneration.from_pretrained(
+    REPO_ID,
+    torch_dtype=torch.float16,
+    quantization_config=quantization_config,
+)
+
+for i in range(10):
+    example = test_dataset[i]
+    test_image = example['image']
+
+    prompt = f"USER: <image>\n{example['prompt']}\nASSISTANT:"
+    inputs = processor(text=prompt, images=[test_image], return_tensors="pt").to("cuda")
+    for k,v in inputs.items():
+        print(k,v.shape)
+
+    generated_ids = model.generate(**inputs, max_new_tokens=MAX_LENGTH)
+
+    # Decode back into text
+    generated_texts = processor.batch_decode(generated_ids, skip_special_tokens=True)
+
+    print("Generated Text:")
+    print(generated_texts)
+    print("GT:")
+    print(example['ground_truth'])
+    
