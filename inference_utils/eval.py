5ae14141f819776809e5e3e1ad8556b2061f4ec8
kushal-10 <kushalkoshti11@gmail.com>
2024-10-11 15:09:53 +0200

    update inference

--- inference_utils/eval.py
+++ inference_utils/eval.py
@@ -1,3 +1,10 @@
 """
 Evaluation strategies
+
+1) Episodic -> Check How many episodes are completed -> Trained on single turn evaluated as single turn.
+ - episodic_success_rate out of 540 episodes (3600 episodes total, 2600 in training)
+ 
+2) Raw -> Overall accuracy
+ - Out of all instances - 2000 instances
 """
+
--- inference_utils/infer.py
+++ inference_utils/infer.py
@@ -1,62 +1,79 @@
-from transformers import AutoProcessor, BitsAndBytesConfig, LlavaForConditionalGeneration
+from transformers import AutoProcessor, BitsAndBytesConfig, AutoModelForVision2Seq
 import torch
 from datasets import load_from_disk
 import os
 from tqdm import tqdm
+import argparse  # Add this import
 
-LEVEL = "easy"
-eval_base = True
-
-MAX_LENGTH = 384
-MODEL_ID = "llava-hf/llava-1.5-7b-hf"
-REPO_ID = MODEL_ID
-hf_dataset = load_from_disk(os.path.join('training_data', f'hf_dataset_{LEVEL}'))
-test_dataset = hf_dataset['test']
-
-if not eval_base:
-    REPO_ID = f"Koshti10/llava-1.5-7b-ft-{LEVEL}"
+# LEVEL = "easy"
+# eval_base = True
+# MODEL_ID = "llava-hf/llava-1.5-7b-hf"
+# REPO_ID = f"Koshti10/llava-1.5-7b-ft-{LEVEL}"
 
 
-processor = AutoProcessor.from_pretrained(MODEL_ID)
+def evaluate(level, model, type='raw'):
+    """
+    Function to evaluate the Finetuned and base models
+    """
+    MAX_LENGTH = 50
+    MODEL_ID = model
+    LEVEL = level
 
-# Define quantization config
-quantization_config = BitsAndBytesConfig(
-    load_in_4bit=True, bnb_4bit_quant_type="nf4", bnb_4bit_compute_dtype=torch.float16
-)
+    hf_dataset = load_from_disk(os.path.join('training_data', f'hf_dataset_{LEVEL}'))
+    test_dataset = hf_dataset['test']
 
-# Load the base model with adapters on top
-model = LlavaForConditionalGeneration.from_pretrained(
-    REPO_ID,
-    torch_dtype=torch.float16,
-    quantization_config=quantization_config,
-)
+    processor = AutoProcessor.from_pretrained(MODEL_ID)
 
-count = 0
-steps = 500
-for i in tqdm(range(steps), desc="Evaluating"):
-    example = test_dataset[i]
-    test_image = example['image']
+    # Define quantization config
+    quantization_config = BitsAndBytesConfig(
+        load_in_4bit=True, bnb_4bit_quant_type="nf4", bnb_4bit_compute_dtype=torch.float16
+    )
 
-    prompt = f"USER: <image>\n{example['prompt']}. Answer in one word only.\nASSISTANT:"
-    inputs = processor(text=prompt, images=[test_image], return_tensors="pt").to("cuda")
+    # Load the base model with adapters on top
+    model = AutoModelForVision2Seq.from_pretrained(
+        MODEL_ID,
+        torch_dtype=torch.float16,
+        quantization_config=quantization_config,
+    )
 
-    generated_ids = model.generate(**inputs, max_new_tokens=MAX_LENGTH)
+    if type == 'raw':
+        count = 0
+        steps = 500
+        for i in tqdm(range(steps), desc="Evaluating"):
+            example = test_dataset[i]
+            test_image = example['image']
 
-    # Decode back into text
-    generated_texts = processor.batch_decode(generated_ids, skip_special_tokens=True)
+            prompt = f"USER: <image>\n{example['prompt']}. Answer in one word only.\nASSISTANT:"
+            inputs = processor(text=prompt, images=[test_image], return_tensors="pt").to("cuda")
 
-    # Extract move
-    move = generated_texts[0].split('ASSISTANT:')[-1].strip()
-   
+            generated_ids = model.generate(**inputs, max_new_tokens=MAX_LENGTH)
 
-    # print("Generated Text:")
-    # print(generated_texts)
-    # print("GT:")
-    # print(example['ground_truth'])
-    print(move.lower(), example['ground_truth'])
+            # Decode back into text
+            generated_texts = processor.batch_decode(generated_ids, skip_special_tokens=True)
 
-    if example['ground_truth'] == move.lower():
-        count += 1
+            # Extract move
+            move = generated_texts[0].split('ASSISTANT:')[-1].strip()
+        
+            print(move.lower(), example['ground_truth'])
+
+            if example['ground_truth'] == move.lower():
+                count += 1
+            
+            print(count/(i+1))
+            print(count, i+1)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Evaluate the Finetuned and base models")
+    parser.add_argument('--level', type=str, default='easy', help='Level of the dataset')
+    parser.add_argument('--model', type=str, required=True, help='Model ID to evaluate, FT/Base HF repo')
+    parser.add_argument('--type', type=str, default='raw', help='Type of evaluation (raw, episodic)')
+
+    args = parser.parse_args()
+
+    evaluate(args.level, args.model, args.type)
+
+"""
+python3 inference_utils/infer.py --level easy --model Koshti10/llava-1.5-7b-ft-pentomino-easy --type raw
+"""
     
-    print(count/(i+1))
-    print(count, i+1)
