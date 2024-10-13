1b7092303cf85b8427d998259b40638f9a6d343a
kushal-10 <kushalkoshti11@gmail.com>
2024-10-13 07:18:32 +0200

    update inference

--- inference_utils/infer.py
+++ inference_utils/infer.py
@@ -3,12 +3,9 @@ import torch
 from datasets import load_from_disk
 import os
 from tqdm import tqdm
-import argparse  # Add this import
-
-# LEVEL = "easy"
-# eval_base = True
-# MODEL_ID = "llava-hf/llava-1.5-7b-hf"
-# REPO_ID = f"Koshti10/llava-1.5-7b-ft-{LEVEL}"
+import argparse
+import pandas as pd
+import logging
 
 
 def evaluate(level, model, type='raw'):
@@ -21,6 +18,7 @@ def evaluate(level, model, type='raw'):
 
     hf_dataset = load_from_disk(os.path.join('training_data', f'hf_dataset_{LEVEL}'))
     test_dataset = hf_dataset['test']
+    steps = len(test_dataset)
 
     processor = AutoProcessor.from_pretrained(MODEL_ID)
 
@@ -36,40 +34,57 @@ def evaluate(level, model, type='raw'):
         quantization_config=quantization_config,
     )
 
-    if type == 'raw':
-        count = 0
-        steps = 500
-        for i in tqdm(range(steps), desc="Evaluating"):
-            example = test_dataset[i]
-            test_image = example['image']
+    if not os.path.exists('results'):
+        os.makedirs('results')
 
-            prompt = f"USER: <image>\n{example['prompt']}. Answer in one word only.\nASSISTANT:"
-            inputs = processor(text=prompt, images=[test_image], return_tensors="pt").to("cuda")
+    predictions = []
+    ground_truths = []
+    image_ids = []
 
-            generated_ids = model.generate(**inputs, max_new_tokens=MAX_LENGTH)
+    check_acc = 0
+    for i in tqdm(range(steps), desc="Evaluating on test dataset"):
+        example = test_dataset[i]
+        test_image = example['image']
+        image_id = example['image_string']
 
-            # Decode back into text
-            generated_texts = processor.batch_decode(generated_ids, skip_special_tokens=True)
+        prompt = f"USER: <image>\n{example['prompt']}. Answer in one word only.\nASSISTANT:"
+        inputs = processor(text=prompt, images=[test_image], return_tensors="pt").to("cuda")
 
-            # Extract move
-            move = generated_texts[0].split('ASSISTANT:')[-1].strip()
-        
-            print(f"Prideiction : {move.lower()} \n\n")
-            print(f"GT : {example['ground_truth']} \n\n")
+        generated_ids = model.generate(**inputs, max_new_tokens=MAX_LENGTH)
 
-            if example['ground_truth'] == move.lower():
-                count += 1
-            
-            # print(count/(i+1))
-            # print(count, i+1)
+        # Decode back into text
+        generated_texts = processor.batch_decode(generated_ids, skip_special_tokens=True)
+
+        # Extract move
+        move = generated_texts[0].split('ASSISTANT:')[-1].strip()
+    
+        predictions.append(move)
+        ground_truths.append(example['ground_truth'])
+        image_ids.append(image_id)
+
+        if move.lower() == example['ground_truth']:
+            check_acc += 1
+
+        if i % 50 == 0:
+            logging.info(f'Accuracy at step {i}: {check_acc/(i+1)}')
+
+    
+    save_name =model.split('/')[-1] + ".csv"
+    infer_data = {
+        "predictions": predictions,
+        "gts": ground_truths,
+        "ids": image_ids
+    }    
+    infer_df = pd.DataFrame(infer_data)
+    infer_df.to_csv(os.path.join('results', save_name), index=False)
+    logging.info(f'Saved dataframe to {os.path.join('results', save_name)}')
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Evaluate the Finetuned and base models")
     parser.add_argument('--level', type=str, default='easy', help='Level of the dataset')
     parser.add_argument('--model', type=str, required=True, help='Model ID to evaluate, FT/Base HF repo')
-    parser.add_argument('--type', type=str, default='raw', help='Type of evaluation (raw, episodic)')
 
     args = parser.parse_args()
 
-    evaluate(args.level, args.model, args.type)
+    evaluate(args.level, args.model)
