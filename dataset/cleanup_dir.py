50f54f2e90f4be1dc2521abf0e3ea1b937d7a798
kushal-10 <kushalkoshti11@gmail.com>
2024-10-22 11:27:54 +0200

    update

--- dataset/cleanup_dir.py
+++
@@ -1,5 +0,0 @@
-"""
-Us eonly when overwriting an already created dataset - better to delete the previous one, before creating a new set for the same level,
-os.rmdir not hardcoded in the main script, as to not accidently delete the dataset
-"""
-
