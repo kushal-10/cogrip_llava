288816fab41fc209afd96bf60de5b99ac5512f35
kushal-10 <kushalkoshti11@gmail.com>
2024-10-24 15:57:56 +0200

    update results

--- plots/fail.py
+++ plots/fail.py
@@ -33,12 +33,15 @@ def get_pos(s):
 
 
 fail_data = np.zeros((18,18))
-
+epic_shape_fails = {}
+epic_colour_fails = {}
 def plot_fails(csv_path):
     df = pd.read_csv(csv_path)
     for i in range(len(df)):
         position = df.iloc[i]['target_position']
         predicted_position = df.iloc[i]['predicted_position']
+        shape = df.iloc[i]['shape']
+        colour = df.iloc[i]['color']
         position = get_pos(position)
         predicted_position = get_pos(predicted_position)
 
@@ -50,6 +53,12 @@ def plot_fails(csv_path):
         valid_positions = [position]
         if not (predicted_position in valid_positions and df.iloc[i]['last_move'] == 'grip'):
             fail_data[position[0], position[1]] += 1
+            if shape not in epic_shape_fails:
+                epic_shape_fails[shape] = 0
+            epic_shape_fails[shape] += 1
+            if colour not in epic_colour_fails:
+                epic_colour_fails[colour] = 0
+            epic_colour_fails[colour] += 1
 
     plt.figure(figsize=(9, 9))
     plt.imshow(fail_data, cmap='cool', interpolation='bilinear', extent=(1, 18, 18, 1))  # Create the heatmap
@@ -59,6 +68,8 @@ def plot_fails(csv_path):
     plt.ylabel('Y Position')  # Label for y-axis
     plt.savefig(f'plots/{csv_path.split("/")[-1].split(".csv")[0]}_fails.png', bbox_inches='tight')  # Save the plot locally
     plt.close()
+    print(epic_shape_fails)
+    print(epic_colour_fails)
 
 
 
@@ -66,6 +77,6 @@ def plot_fails(csv_path):
 
 if __name__ == "__main__":
     dacc_folder = "results"
-    dacc_csvs = ["results/llava-1.5-13b-hf-ft-2ep_easy.csv"]
+    dacc_csvs = ["results/llava-1.5-7b-hf-ft_easy.csv"]
     for csv_path in dacc_csvs:
         plot_fails(csv_path)
